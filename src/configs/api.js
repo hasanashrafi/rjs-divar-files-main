414fff1c6534d5f63dca1e1c77e80f5c3606f35f
Hasan Ashrafi <yasashrafi@gmail.com>
2024-02-15 13:03:06 +0330

    refactor code and solve problem

--- src/configs/api.js
+++ src/configs/api.js
@@ -1,10 +1,16 @@
 import axios from "axios";
 
+
 const api = axios.create({
-    baseURL: import.meta.env.VITE_BASE_URL,
+    baseURL: 'http://localhost:3400/' ,
     headers: {
         "Content-Type": "application/json",
     },
 });
 
-export default api
+export { api }
+
+
+
+
+// import.meta.env.VITE_BASE_URL
