e7c6165bdbaeb8b30d50c8507c1bacccd17d76f5
Hasan Ashrafi <yasashrafi@gmail.com>
2024-02-15 13:02:23 +0330

    create checkOtp form component and get data and show data in component

--- src/components/templates/CheckOtpForm.jsx
+++ src/components/templates/CheckOtpForm.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
+import { checkOtp } from '../../services/auth';
 
-function CheckOtpForm() {
+
+function CheckOtpForm({ code, setCode, setStep, mobile }) {
+  const submitHandler = async (e) => {
+    e.preventDefault();
+
+    if (code.length !== 5) return
+
+    const { response, error } = await checkOtp(mobile, code)
+
+    if (response) {
+      console.log(response);
+    }
+    if (error) console.log(error.response);
+
+    console.log({response,error});
+  }
   return (
-    <div>
-      CheckOtpForm
-    </div>
+    <form onSubmit={submitHandler}>
+      <p>تایید کد پیامک شده</p>
+      <span>کد ارسال شده به شماره {mobile}را وارد کنید.</span>
+      <label htmlFor='input'>کد تایید را وارد کنید </label>
+      <input
+        type='text'
+        placeholder='کد تایید'
+        value={code}
+        onChange={e => setCode(e.target.value)} />
+      <button type='submit'>ورود</button>
+      <button onClick={() => setStep(1)}>تغییر شماره موبایل</button>
+    </form>
   );
 }
 
