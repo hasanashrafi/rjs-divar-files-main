1227f52066004a28b0d956f488057b45121ac238
Hasan Ashrafi <yasashrafi@gmail.com>
2024-02-17 10:19:47 +0330

    create 404 component

---
+++ src/pages/404.jsx
@@ -0,0 +1,11 @@
+
+function PageNotFound() {
+    return (
+        <div>
+            <h2>Error 404 </h2>
+            <h3>Page Not Found</h3>
+        </div>
+    );
+}
+
+export default PageNotFound;
