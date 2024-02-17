bc5f7b2aa09a8d1f301c30da839c6baa053956af
Hasan Ashrafi <yasashrafi@gmail.com>
2024-02-17 10:16:28 +0330

    create different way to access paths

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import paths from './src/constants/paths'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      ...paths.reduce((acc, cur) => ({
+        ...acc,
+        [cur]: `/${cur === "src" ? cur : "src/" + cur}`
+      }), "")
+    }
+  }
 })
+
+
+// src:"/src",
+//       services:"/src/services",
+//       utils:"/src/utils",
+//       configs:"/src/configs",
+//       pages:"/src/pages",
+//       styles:"/src/styles",
+//       assets:"/src/assets",
+//       components:"/src/components",
+//       layouts:"/src/layouts",
+//       router:"/src/router"
