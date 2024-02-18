afaf5874a2866f466d57f9eff0c127ceeb745c8e
Hasan Ashrafi <yasashrafi@gmail.com>
2024-02-18 10:57:04 +0330

    create router function

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,13 +1,22 @@
 import { Route, Routes } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
 
 import PageNotFound from "pages/404";
 import AdminPage from "pages/AdminPage";
 import AuthPage from "pages/AuthPage";
 import DashboardPage from "pages/DashboardPage";
 import HomePage from "pages/HomePage";
+import getProfile from "services/user";
 
 
 function Router() {
+    const { data, isLoading,error } = useQuery({
+       queryKey: ["profile"],
+       queryFn: getProfile()
+    })
+    console.log({ data, isLoading ,error});
+
+    // if(isLoading) return <h1>در حال بروزرسانی </h1>
     return (
         <Routes>
             <Route path="/" element={<HomePage />} />
