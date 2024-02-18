5f6012b273814ea09006d1d68cc0fb28efc830b9
Hasan Ashrafi <yasashrafi@gmail.com>
2024-02-18 10:53:07 +0330

    added react devtools

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import { BrowserRouter} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import Router from "./router/Router";
+import defaultOptions from "./configs/reactQueryConfig";
 
 function App() {
+  const queryClint = new QueryClient(defaultOptions)
   return (
-    <BrowserRouter>
-      <Router/>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClint}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   );
 }
 
