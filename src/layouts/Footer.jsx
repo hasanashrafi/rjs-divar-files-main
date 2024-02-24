f331fe90f4b7136b9c3cda95a24b2c3089ed3546
Hasan Ashrafi <yasashrafi@gmail.com>
2024-02-24 13:51:08 +0330

    create footer component

---
+++ src/layouts/Footer.jsx
@@ -0,0 +1,11 @@
+import styles from "./Footer.module.css"
+
+function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <p>DeVeloped By HsN</p>
+    </footer>
+  );
+}
+
+export default Footer;
