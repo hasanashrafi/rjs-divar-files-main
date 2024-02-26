98ac165f0d08101ddf1e13eab50e8bd44eaeca06
Hasan Ashrafi <yasashrafi@gmail.com>
2024-02-26 18:37:59 +0330

    added form to component and submit form data

--- src/components/templates/CategoryForm.jsx
+++ src/components/templates/CategoryForm.jsx
@@ -1,11 +1,31 @@
-import React from 'react';
+import { useState } from "react";
+
+import styles from "./CategoryForm.module.css"
 
 function CategoryForm() {
-  return (
-    <div>
-      CategoryForm
-    </div>
-  );
+    const [form, setForm] = useState({ name: "", slug: "", icon: "" })
+    const changeHandler = (e) => {
+        setForm({ ...form, [e.target.name]: e.target.value })
+    }
+    const submitHandler = (e) => {
+        e.preventDefault()
+        console.log(form);
+    }
+
+    return <form onChange={changeHandler} onSubmit={submitHandler} className={styles.form}>
+        <h3>دسته بندی جدید</h3>
+        <p>error</p>
+
+        <label htmlFor='name'>اسم دسته بندی</label>
+        <input type='text' name='name' id='name' />
+
+        <label htmlFor='slug'> اسلاگ </label>
+        <input type='text' name='slug' id='slug' />
+
+        <label htmlFor='icon'> آیکون </label>
+        <input type='text' name='icon' id='icon' />
+        <button type="submit">ایجاد</button>
+    </form>
 }
 
 export default CategoryForm;
