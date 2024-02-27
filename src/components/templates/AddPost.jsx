01786fa10f9280d44c9c61c1f72bf73c4f9c885e
Hasan Ashrafi <yasashrafi@gmail.com>
2024-02-27 17:33:47 +0330

    create addPost component and post data

---
+++ src/components/templates/AddPost.jsx
@@ -0,0 +1,83 @@
+import axios from "axios";
+import { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+
+import { getCategory } from "services/admin";
+import { getCookie } from "utils/cookie";
+
+import styles from "./AddPost.module.css"
+function AddPost() {
+    const [form, setForm] = useState({
+        title: "",
+        content: "",
+        category: "",
+        city: "",
+        amount: null,
+        images: null,
+
+    });
+
+    const { data } = useQuery({
+        queryKey: ["get-categories"],
+        queryFn: getCategory,
+    });
+
+    const changeHandler = (e) => {
+        const name = e.target.name;
+        if (name !== "images") {
+            setForm({ ...form, [name]: e.target.value })
+        } else {
+            setForm({ ...form, [name]: e.target.files[0] })
+        }
+    }
+
+    const addHandler = (e) => {
+        e.preventDefault()
+        const formData = new FormData();
+
+        for (let i in form) {
+            formData.append(i, form[i])
+        }
+        const token = getCookie("accessToken")
+        axios.post(`${import.meta.env.VITE_BASE_URL}post/create`, formData, {
+            headers: {
+                "Content-Type": "multipart/form-data",
+                Authorization: `bearer ${token}`
+            }
+        }).then((res) => toast.success(res.data.message))
+            .catch((error) => toast.error("مشکلی پیش آمده است."))
+    }
+
+    return (
+        <form onChange={changeHandler} className={styles.form}>
+            <h3>افزودن آگهی</h3>
+            <label htmlFor="title">عنوان آگهی</label>
+            <input type="text" name="title" id="title" />
+
+            <label htmlFor="content">توضیحات آگهی</label>
+            <textarea name="content" id="content" />
+
+            <label htmlFor="amount">قیمت </label>
+            <input type="number" name="amount" id="amount" />
+
+            <label htmlFor="city">شهر </label>
+            <input type="text" name="city" id="city" />
+
+            <label htmlFor="category">دسته بندی </label>
+            <select id="category" name="category">
+                {data?.data.map((i) => (
+                    <option key={i._id} value={i._id}>{i.name}</option>
+                ))}
+            </select>
+
+            <label htmlFor="images">عکس </label>
+            <input type="file" name="images" id="images" />
+            <button onClick={addHandler}>ایجاد</button>
+           
+        </form>
+    );
+}
+
+export default AddPost;
+
