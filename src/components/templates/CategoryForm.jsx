c49fe4514fb87907e3851fa4208a22c5ee1cf68b
Hasan Ashrafi <yasashrafi@gmail.com>
2024-02-27 17:40:42 +0330

    mutate data

--- src/components/templates/CategoryForm.jsx
+++ src/components/templates/CategoryForm.jsx
@@ -1,20 +1,32 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useState } from "react";
+
+import { addCategory } from "services/admin";
 
 import styles from "./CategoryForm.module.css"
 
 function CategoryForm() {
+    const queryClient = useQueryClient()
     const [form, setForm] = useState({ name: "", slug: "", icon: "" })
     const changeHandler = (e) => {
         setForm({ ...form, [e.target.name]: e.target.value })
     }
+
+    const { mutate, isLoading, error, data } = useMutation(addCategory, {
+        onSuccess: () => queryClient.invalidateQueries("get-categories"),
+    })
+    console.log({ data, isLoading, error });
+
     const submitHandler = (e) => {
         e.preventDefault()
-        console.log(form);
+        if (!form.name || !form.slug || !form.icon) return
+        mutate(form);
     }
 
     return <form onChange={changeHandler} onSubmit={submitHandler} className={styles.form}>
         <h3>دسته بندی جدید</h3>
-        <p>error</p>
+        {!!error && <p>مشکلی پیش آمده</p>}
+        {data?.status === 201 && <p>دسته بندی با موفقیت اضافه شد</p>}
 
         <label htmlFor='name'>اسم دسته بندی</label>
         <input type='text' name='name' id='name' />
@@ -24,7 +36,7 @@ function CategoryForm() {
 
         <label htmlFor='icon'> آیکون </label>
         <input type='text' name='icon' id='icon' />
-        <button type="submit">ایجاد</button>
+        <button type="submit" disabled={isLoading}>ایجاد</button>
     </form>
 }
 
