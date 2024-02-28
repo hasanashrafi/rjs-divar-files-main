26f811de42e3957c2d122e0f8b1770eda8257e09
Hasan Ashrafi <yasashrafi@gmail.com>
2024-02-28 15:27:33 +0330

    get posts and categories

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,34 @@
+import { useQuery } from "@tanstack/react-query";
+
+import Main from "components/templates/Main";
+import SideBar from "components/templates/SideBar";
+import { getAllPosts } from "services/user";
+import Loader from "components/modules/Loader";
+import { getCategory } from "src/services/admin";
 
 
 function HomePage() {
+  const style = { display: "flex" }
+  
+  const { data: posts, isLoading: postLoading } = useQuery({
+    queryKey: ["post-list"],
+    queryFn: getAllPosts
+  })
+  const { data: categories, isLoading: categoryLoading } = useQuery({
+    queryKey: ["get-categories"],
+    queryFn: getCategory,
+  });
+
   return (
-    <div>
-      Home
-    </div>
+    <>
+      {
+        postLoading || categoryLoading ? <Loader /> : (
+          <div style={style}>
+            <SideBar categories={categories} />
+            <Main posts={posts} />
+          </div>
+        )}
+    </>
   );
 }
 
