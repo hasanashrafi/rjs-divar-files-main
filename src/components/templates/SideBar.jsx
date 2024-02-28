1ebb3e25cc9890ca32aaa6a6ca7c73bd0b829cdd
Hasan Ashrafi <yasashrafi@gmail.com>
2024-02-28 15:25:27 +0330

    create side bar component

---
+++ src/components/templates/SideBar.jsx
@@ -0,0 +1,19 @@
+
+import styles from "./SideBar.module.css"
+function SideBar({ categories }) {
+    return <div className={styles.sideBar}>
+        <h4>دسته ها</h4>
+        <ul>
+            {
+                categories.data.map((category) =>
+                    <li key={category._id}>
+                        <img src={`${category.icon}.svg`} />
+                        <p>{category.name}</p>
+                    </li>
+                    )}
+        </ul>
+    </div>
+
+}
+
+export default SideBar;
