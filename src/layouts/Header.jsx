f61014179a6e14d9f1193c93e525d740828d031d
Hasan Ashrafi <yasashrafi@gmail.com>
2024-02-28 15:26:15 +0330

    refactor codes

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,13 +1,23 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 import styles from "./Header.module.css"
+import { deleteCookie } from "src/utils/cookie";
+import { useQuery } from "@tanstack/react-query";
+import { getProfile } from "src/services/user";
+
 
 function Header() {
+    const navigate = useNavigate()
+  const { refetch } = useQuery({
+    queryKey: ["profile"],
+    queryFn: getProfile,
+  })
     return (
+
         <header className={styles.header}>
             <div>
                 <Link to="/">
-                    <img src="divar.svg" className={styles.logo}/>
+                    <img src="divar.svg" className={styles.logo} />
                 </Link>
                 <span>
                     <img src="location.svg" />
@@ -15,12 +25,18 @@ function Header() {
                 </span>
             </div>
             <div>
-                <Link to="/auth">
+                <Link to="/auth"  >
                     <span>
                         <img src="profile.svg" />
                         <p>دیوار من</p>
                     </span>
+
+                    <div className={styles.navBar} onClick={deleteCookie}>
+                        <Link to="/auth" > خروج</Link>
+                    </div>
+
                 </Link>
+
                 <Link to="/dashboard" className={styles.button}>
                     ثبت آگهی
                 </Link>
